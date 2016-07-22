48765b3ee86aeaecfb6d948fa5255b84d013493f
ISMAEL HENRIQUE DOS SANTOS VAZ <15160210@CFP127ALUNO.LOCAL>
2016-07-22 16:44:29 -0300

    comit inicial

---
+++ src/br/com/gere/controller/ContatoController.java
@@ -0,0 +1,65 @@
+package br.com.gere.controller;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+import br.com.gere.helper.MySqlConection;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class ContatoController implements Initializable{
+	@FXML
+	TextField txtNome, txtTelefone;
+	
+	@FXML
+	Button btnInserir;
+	
+	@FXML
+	ListView<String> lstView;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		btnInserir.setOnAction(l-> Inserir());
+		PreencherLista();
+	}
+	public void Inserir(){
+		if(!txtNome.getText().isEmpty() && !txtTelefone.getText().isEmpty()){
+			Connection con = MySqlConection.ConectarDB();
+			
+			String sql = "INSERT INTO contact (name, phone) value (?, ?)";
+			
+			PreparedStatement parametros;
+			
+			try {
+				parametros = con.prepareStatement(sql);
+				parametros.setString(1, txtNome.getText());
+				parametros.setString(2, txtTelefone.getText());
+				
+				parametros.executeUpdate();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			PreencherLista();
+		}
+	}
+	public void PreencherLista(){
+		Connection con = MySqlConection.ConectarDB();
+		lstView.getItems().clear();
+		String sql = "select * from contact;";
+		
+		try {
+			ResultSet rs = con.createStatement().executeQuery(sql);
+			while(rs.next()){
+				String contato = rs.getString("name")+"-"+rs.getString("phone");
+				lstView.getItems().add(contato);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/br/com/gere/helper/MySqlConection.java
@@ -0,0 +1,20 @@
+package br.com.gere.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class MySqlConection {
+
+	public static Connection ConectarDB(){
+		Connection con = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://10.107.134.38/contatos", "root", "root");
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		return con;
+	}
+}
